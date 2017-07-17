007e29079b6ff04b7d7bb0f8e2217d109af817a4
Markus  Gray <syncwarellc@gmail.com>
2017-07-17 15:39:46 -0400

    Layout changes

--- resources/views/contracts/index.blade.php
+++ resources/views/contracts/index.blade.php
@@ -4,44 +4,48 @@
 
 	<div class="container">
 		<div class="row">
-			<h2>Contracts</h2>
-			<a href="/contracts/create" class="btn btn-primary pull-right" style="margin-top: -46px;">Add Contract</a>
-			<div class="panel panel-default">
-				<table class="table table-striped table-bordered">
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>NAME</th>
-							<th>ACTIONS</th>
-						</tr>
-					</thead>
-					<tbody>
-						@if($contracts->count())
-							@foreach($contracts as $contract)
-								
+			<div class="col-md-12">
+				<h2>Contracts</h2>
+				<a href="/contracts/create" class="btn btn-primary pull-right" style="margin-top: -46px;">Add Contract</a>
+				<div class="panel panel-default">
+					<div class="table-responsive">
+						<table class="table table-striped table-bordered">
+							<thead>
 								<tr>
-									<td>
-										{{ $contract->id }}
-									</td>
-									<td>
-										{{ $contract->name }}
-									</td>
-									<td>
-										<a href="/contracts/{{ $contract->id }}/edit">Edit</a>
-									</td>
+									<th>ID</th>
+									<th>NAME</th>
+									<th>ACTIONS</th>
 								</tr>
+							</thead>
+							<tbody>
+								@if($contracts->count())
+									@foreach($contracts as $contract)
+										
+										<tr>
+											<td>
+												{{ $contract->id }}
+											</td>
+											<td>
+												{{ $contract->name }}
+											</td>
+											<td>
+												<a href="/contracts/{{ $contract->id }}/edit">Edit</a>
+											</td>
+										</tr>
 
 
-							@endforeach
-						@else
-							<tr>
-								<td colspan="3">
-									<p align="center">You currently have no contracts. <a href="/contracts/create">Create a contract</a></p>
-								</td>
-							</tr>
-						@endif
-					</tbody>
-				</table>
+									@endforeach
+								@else
+									<tr>
+										<td colspan="3">
+											<p align="center">You currently have no contracts. <a href="/contracts/create">Create a contract</a></p>
+										</td>
+									</tr>
+								@endif
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- resources/views/lots/index.blade.php
+++ resources/views/lots/index.blade.php
@@ -4,44 +4,48 @@
 
 	<div class="container">
 		<div class="row">
-			<h2>Lots</h2>
-			<a href="/lots/create" class="btn btn-primary pull-right" style="margin-top: -46px;">Add Lot</a>
-			<div class="panel panel-default">
-				<table class="table table-striped table-bordered">
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>NAME</th>
-							<th>ACTIONS</th>
-						</tr>
-					</thead>
-					<tbody>
-						@if($lots->count())
-							@foreach($lots as $lot)
-								
+			<div class="col-md-12">
+				<h2>Lots</h2>
+				<a href="/lots/create" class="btn btn-primary pull-right" style="margin-top: -46px;">Add Lot</a>
+				<div class="panel panel-default">
+					<div class="table-responsive">
+						<table class="table table-striped table-bordered">
+							<thead>
 								<tr>
-									<td>
-										{{ $lot->id }}
-									</td>
-									<td>
-										{{ $lot->name }}
-									</td>
-									<td>
-										<a href="/lots/{{ $lot->id }}/edit">Edit</a>
-									</td>
+									<th>ID</th>
+									<th>NAME</th>
+									<th>ACTIONS</th>
 								</tr>
+							</thead>
+							<tbody>
+								@if($lots->count())
+									@foreach($lots as $lot)
+										
+										<tr>
+											<td>
+												{{ $lot->id }}
+											</td>
+											<td>
+												{{ $lot->name }}
+											</td>
+											<td>
+												<a href="/lots/{{ $lot->id }}/edit">Edit</a>
+											</td>
+										</tr>
 
 
-							@endforeach
-						@else
-							<tr>
-								<td colspan="3">
-									<p align="center">You currently have no lots. <a href="/lots/create">Create a lot</a></p>
-								</td>
-							</tr>
-						@endif
-					</tbody>
-				</table>
+									@endforeach
+								@else
+									<tr>
+										<td colspan="3">
+											<p align="center">You currently have no lots. <a href="/lots/create">Create a lot</a></p>
+										</td>
+									</tr>
+								@endif
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>

272960877759963ac5411fb87e21dd2cee6fce9b
Markus  Gray <syncwarellc@gmail.com>
2017-12-11 11:39:07 -0500

    Search Order

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,13 +13,13 @@ class SearchController extends Controller
     	$by = $request->by;
 
     	if($by == "tow_number"){
-    		$tows = Tow::parents()->where('tow_number', $term)->lastUpdated()->get();
+    		$tows = Tow::parents()->lastUpdated()->where('tow_number', $term)->get();
     	}
     	elseif($by == "tags"){
-    		$tows = Tow::parents()->where('tags', 'like', '%' . $term . '%')->lastUpdated()->get();
+    		$tows = Tow::parents()->lastUpdated()->where('tags', 'like', '%' . $term . '%')->get();
     	}
         elseif($by == "vin"){
-            $tows = Tow::parents()->where('vin', 'like', '%' . $term . '%')->lastUpdated()->get();
+            $tows = Tow::parents()->lastUpdated()->where('vin', 'like', '%' . $term . '%')->get();
         }  	
 
     	return view('search', [
